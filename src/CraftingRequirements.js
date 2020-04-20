987c5a16891c5655c82c676a59edd38ee1e19b13
Kyle Shrader <kyle.r.shrader@gmail.com>
2020-04-20 16:27:49 -0700

    Adding copy button to craft requirements

--- src/CraftingRequirements.js
+++ src/CraftingRequirements.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Segment, List, Header } from 'semantic-ui-react'
+import React, { useEffect, useState, useRef } from 'react'
+import { Segment, List, Header, Icon, Popup } from 'semantic-ui-react'
+import copy from 'copy-to-clipboard'
 
 export default function CraftingRequirements({items}) {
   const [materials, setMaterials] = useState({})
+  const [copyOpen, setCopyOpen] = useState(false)
+  const listDiv = useRef(null)
 
   useEffect(() => {
     const mats = {}
@@ -16,16 +19,49 @@ export default function CraftingRequirements({items}) {
     setMaterials(mats)
   }, [items])
 
+  useEffect(() => {
+    if(copyOpen) {
+      copy(Object.keys(materials).reduce(
+        (prev, key) =>`${prev}\r\n${materials[key]}x ${key}`,
+        '').replace('\r\n', '')
+      )
+      const timeout = setTimeout(() => {
+        console.log('timed out')
+        setCopyOpen(false)
+      }, 500);
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  }, [copyOpen])
+
   return (
     <Segment basic>
       <List>
-          <Header>Crafting will require:</Header>
+        <Popup
+          content='Copied!'
+          size='small'
+          inverted
+          open={copyOpen}
+          trigger={<Icon
+            name='copy'
+            onClick={(e) => setCopyOpen(true)}
+            style={{
+              float: 'left',
+              cursor: 'pointer'
+            }} />}
+        />
+        <Header>Crafting will require:</Header>
+        <div ref={listDiv}>
         {Object.keys(materials).map(key => (
           <List.Item className='recipe'>
-            <span className='ingredientQty'>{materials[key]}x</span>
-            <span className='ingredientName'>{key}</span>
+            <div style={{display: 'inline-block'}}>
+              <span className='ingredientQty'>{materials[key]}x&nbsp;</span>
+              <span className='ingredientName'>{key}</span>
+            </div>
           </List.Item>
         ))}
+        </div>
       </List>
     </Segment>
   )
