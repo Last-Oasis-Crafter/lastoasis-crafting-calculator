caf0b46e674488336004704f4c8aa1605a72d5f3
Kyle Shrader <kyle.r.shrader@gmail.com>
2020-04-24 23:40:58 -0700

    Adding react-ga and google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { useReducer } from 'react';
+import ReactGA from 'react-ga';
+
 
 import './App.css';
 import ItemSidebar from './ItemSidebar'
 import CraftingReducer from './CraftingReducer';
 import CraftingSection from './CraftingSection';
+
+const trackingId = "UA-63206624-2";
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  page: '/',
+  anonymizeIp: true
+});
+ReactGA.pageview('/');
 
 function App() {
   const [crafting, craftingDispatch] = useReducer(CraftingReducer, [])
