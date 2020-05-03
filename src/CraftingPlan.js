6fe7e8e686295776535ef47676b0cf78e047eeb5
RodEsp <rod.esp.m.g@gmail.com>
2020-05-03 00:07:37 -0400

    Fix clear all button alignment

--- src/CraftingPlan.js
+++ src/CraftingPlan.js
@@ -3,19 +3,25 @@ import { Accordion, Button, Header, Segment } from 'semantic-ui-react'
 
 import CraftingCard from './CraftingCard'
 
-export default function CraftingPlan({items, dispatch}) {
+export default function CraftingPlan ({ items, dispatch }) {
 
   return (
     <Segment basic>
-      <Header>Crafting:<Button content='Clear All' floated='right' size='mini' compact onClick={e => dispatch({type: 'REMOVE_ALL'})}></Button></Header>
+      <Header
+        style={{
+          width: '600px'
+        }}>
+        Crafting:
+        <Button content='Clear All' floated='right' size='mini' compact onClick={e => dispatch({ type: 'REMOVE_ALL' })} />
+      </Header>
       <Accordion styled>
-      {items.map((item, index) =>
-        <CraftingCard
-          key={item.name}
-          item={item}
-          dispatch={dispatch}
-          index={index} />
-      )}
+        {items.map((item, index) =>
+          <CraftingCard
+            key={item.name}
+            item={item}
+            dispatch={dispatch}
+            index={index} />
+        )}
       </Accordion>
     </Segment>
   )
