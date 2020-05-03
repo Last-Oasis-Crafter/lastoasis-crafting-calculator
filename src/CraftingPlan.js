ba6794eb7b1f823703ac0546f488ddc4f72b84c8
RodEsp <rod.esp.m.g@gmail.com>
2020-05-02 22:19:33 -0400

    Clear All button

--- src/CraftingPlan.js
+++ src/CraftingPlan.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Accordion, Segment, Header } from 'semantic-ui-react'
+import { Accordion, Button, Header, Segment } from 'semantic-ui-react'
+
 import CraftingCard from './CraftingCard'
 
 export default function CraftingPlan({items, dispatch}) {
 
   return (
     <Segment basic>
-      <Header>Crafting:</Header>
+      <Header>Crafting:<Button content='Clear All' floated='right' size='mini' compact onClick={e => dispatch({type: 'REMOVE_ALL'})}></Button></Header>
       <Accordion styled>
       {items.map((item, index) =>
         <CraftingCard
--- src/CraftingReducer.js
+++ src/CraftingReducer.js
@@ -1,16 +1,16 @@
-export default function(state, action) {
-  switch(action.type) {
+export default function (state, action) {
+  switch (action.type) {
     case 'ADD_ITEM':
       const existingIndex = state.findIndex(item => item.name === action.item.name)
       const newState = state.slice()
-      if(existingIndex < 0) {
+      if (existingIndex < 0) {
         newState.push({
           ...action.item,
           count: 1
         })
         return newState
       } else {
-        return newState.map(item => 
+        return newState.map(item =>
           item.name === action.item.name ? {
             ...item,
             count: item.count + 1 > 999999 ? 999999 : item.count + 1
@@ -20,16 +20,19 @@ export default function(state, action) {
       let count = action.count
       count = count > 999999 ? 999999 : count
       count = count < 1 ? 1 : count
-      if(count > 0 ) {
+      if (count > 0) {
         const newState = state.slice()
-        return newState.map(item => 
+        return newState.map(item =>
           item.name === action.name ? {
             ...item,
             count: count
           } : item)
       }
+      break;
     case 'REMOVE_ITEM':
       return state.slice().filter(item => item.name !== action.name)
+    case 'REMOVE_ALL':
+      return state.slice(0, 0)
     default:
       throw new Error('Reached default state of CraftingReducer ' + JSON.stringify(action))
   }
