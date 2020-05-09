46ded018aee05be054f2965040e7c8d8328b4ba7
RodEsp <rod.esp.m.g@gmail.com>
2020-05-09 14:17:54 -0400

    Sort JSON by category and write it out to file

--- scripts/buildItemsJSON.js
+++ scripts/buildItemsJSON.js
@@ -3,6 +3,12 @@ const ObjGen = require('objgen/objgen').ObjGen;
 
 const itemObjGen = fs.readFileSync('./src/itemObjGen.txt', 'utf8');
 
-const itemsJSON = ObjGen.xJson(itemObjGen, {numSpaces: 2});
+const itemsJSON = JSON.parse(ObjGen.xJson(itemObjGen, {numSpaces: 2}));
 
-fs.writeFileSync('./src/items.json', itemsJSON, 'utf8');
+// Sort Alphabetically by item.name
+itemsJSON.sort((itemA, itemB) => itemA.name.localeCompare(itemB.name));
+
+// Sort Alphabetically by item.category
+itemsJSON.sort((itemA, itemB) => itemA.category.localeCompare(itemB.category));
+
+fs.writeFileSync('./src/items.json', JSON.stringify(itemsJSON, null, '\t'), 'utf8');
