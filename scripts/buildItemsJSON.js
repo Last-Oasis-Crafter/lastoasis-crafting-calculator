8101b8ca2eb127a397a4387fa73aa9b29e134942
Rodrigo Espinosa <rod.esp.m.g@gmail.com>
2020-05-10 00:07:23 -0400

    Automatically create items.json using `npm run objgen` (#55)
    * Automatically create items.json using `npm run objgen`
    
    * Sort JSON by category and write it out to file
    
    * Update contributing instructions in README
    
    * Change from tabs to spaces for the items.json generation

---
+++ scripts/buildItemsJSON.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const ObjGen = require('objgen/objgen').ObjGen;
+
+const itemObjGen = fs.readFileSync('./src/itemObjGen.txt', 'utf8');
+
+const itemsJSON = JSON.parse(ObjGen.xJson(itemObjGen, {numSpaces: 2}));
+
+// Sort Alphabetically by item.name
+itemsJSON.sort((itemA, itemB) => itemA.name.localeCompare(itemB.name));
+
+// Sort Alphabetically by item.category
+itemsJSON.sort((itemA, itemB) => itemA.category.localeCompare(itemB.category));
+
+fs.writeFileSync('./src/items.json', JSON.stringify(itemsJSON, null, 2), 'utf8');
